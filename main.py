6ed2c20300ddad46960da58b7570df42636f1b3b
Mark S. Brown <sandcalc@gmail.com>
2019-05-21 14:10:14 +0100

    make it python3.5 compatible

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from collections import namedtuple
-from random import choices, shuffle
+from random import choice, shuffle
 import os
 from time import sleep, time
 import curses
 import shutil
- 
+
 def clear_screen():
   os.system('cls' if os.name == 'nt' else 'clear')
- 
+
 class Shots:
   def __init__(self, filename='shots.txt'):
     self.shots = self.list_of_shots(filename)
     shuffle(self.shots)
-         
+
   @staticmethod
   def list_of_shots(filename):
     Shot = namedtuple('shot', ('number', 'name', 'ingredients'))
@@ -22,31 +22,31 @@ class Shots:
         row = row.strip('\n')
         new_shot = Shot(*row.split(';'))
         shots.append(new_shot)
- 
+
     return shots
-     
+
   def random_shots(self, k=1):
-    return choices(self.shots, k=k)
-     
+    return [choice(self.shots) for _ in range(k)]
+
   def choose_random_shots(self, duration=2, delay=0.08, k=1):
     lw = shutil.get_terminal_size((80, 20)).columns
     time_to_end = time() + duration
- 
+
     while time() < time_to_end:
       sleep(delay)
       shots = self.random_shots(k)
-         
+
       for shot in shots:
         display_text = '\n'
-        display_text += f' {shot.number}: {shot.name} '.center(lw, '=')
+        display_text += '{shot.number}: {shot.name} '.format(shot.number,shot.name).center(lw, '=')
         display_text += '\n'
         display_text += shot.ingredients.center(lw)
         display_text += '\n'
-        display_text += f''.center(lw, '=')
-        
+        display_text += ''.center(lw, '=')
+
       clear_screen()
       print(display_text)
- 
+
 def main(win):
   win.nodelay(True)
   shots = Shots()
