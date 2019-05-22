e2bed329deed489cb7121289d8beb76b0377cd7d
josephwolf <josephwolf.uk@gmail.com>
2019-05-22 01:10:59 +0100

    Signed

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ class Shots:
         display_text += shot.ingredients.center(lw)
         display_text += '\n'
         display_text += ''.center(lw, '=')
+        display_text += '\n'
+        display_text += '\n'
+        display_text += 'HACK THE WORLD. - JW'
+
         
 
       clear_screen()
